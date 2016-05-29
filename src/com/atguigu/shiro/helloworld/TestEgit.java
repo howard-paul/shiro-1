366a2b619d212424cb165026300b775de995ac21
paul <paul@hrb-pc>
2016-05-29 17:49:05 +0800

    test overwrite

--- src/com/atguigu/shiro/helloworld/TestEgit.java
+++ src/com/atguigu/shiro/helloworld/TestEgit.java
@@ -6,5 +6,6 @@ public class TestEgit {
 		System.out.println("hello test01");
 		System.out.println("local commit");
 		System.out.println("other programmer commit");
+		System.out.println("test overwrite");
 	}
 }
